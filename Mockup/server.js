beef71bd23c8a2a86e57278ee30318dcc77dd259
VisshaalS <40528586+VisshaalS@users.noreply.github.com>
2021-01-24 21:29:58 -0500

    Added index.html and some basic text

--- Mockup/server.js
+++ Mockup/server.js
@@ -1,8 +1,13 @@
 'use strict';
 var http = require('http');
+var fs = require('fs');
 var port = process.env.PORT || 1337;
 
+
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    fs.readFile('index.html', function (err, html) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(html);
+        res.end();
+    });
 }).listen(port);
