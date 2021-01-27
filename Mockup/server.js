bb5806c23c2917d6f64f3c79e3e2e12be0816930
Kyuzo <tekitoh.otoko@gmail.com>
2021-01-27 14:32:44 -0500

    changed some text and instant feedback; also fixed images

--- Mockup/server.js
+++ Mockup/server.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
-    fs.readFile('index.html', function (err, html) {
+    fs.readFile('Steve_index.html', function (err, html) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(html);
         res.end();
