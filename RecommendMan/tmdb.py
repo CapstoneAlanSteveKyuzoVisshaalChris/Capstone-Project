3af4cdcfc2c9a72e35c44aa2a4e7b1a10608d4f6
Kyuzo <tekitoh.otoko@gmail.com>
2021-02-18 00:35:53 -0500

    improved genre, possible to add multiple keywords now

--- RecommendMan/tmdb.py
+++ RecommendMan/tmdb.py
@@ -37,14 +37,18 @@ def tmdb(inputValue):
     # print(size)
     # print(response["output"]["entities"])
     # GENRE
-    genre = response["output"]["entities"][0]["value"]
+    genre=""
+    for word in response["output"]["entities"]:
+        if word.get("entity")=="genre":
+           genre = word.get("value")
+           break
     # KEYWORD
     keywords = []
     i = 1
-    while (i < size):
-        # print(i)
-        keywords.append(response["output"]["entities"][i]["value"])
-        i += 1
+    for word in response["output"]["entities"]:
+        if word.get("entity")=="keywords":
+           keywords.append(word.get("value"))
+
     # print(keywords)
 
     response = assistant.delete_session(
@@ -63,6 +67,22 @@ def tmdb(inputValue):
             search = url + name
             return requests.get(search).json()
 
+        def searchGenre(self,genre):
+            url = 'https://api.themoviedb.org/3/genre/movie/list?api_key=' + self.key + '&query='
+            #print("genre: '", genre, "'")
+            if ' ' in genre:
+                genre.replace(' ','%20')
+            genre = genre.capitalize()
+            search = url + genre
+            req = requests.get(search).json()
+            i = 0
+            id = 0
+            while i < len(req["genres"]):
+                if req["genres"][i].get("name") == genre:
+                    id = req["genres"][i].get("id")
+                i+=1
+            return id
+
         def searchKeyword(self, keyword):
             url = 'https://api.themoviedb.org/3/search/keyword?api_key=' + self.key + '&query='
             if ' ' in keyword:
@@ -70,31 +90,38 @@ def tmdb(inputValue):
             search = url + keyword
             return requests.get(search).json()
 
-        def discover(self, keywordID):
-            url = 'https://api.themoviedb.org/3/discover/movie?api_key=' + self.key + '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_keywords='
+        def discover(self,genreID,keywordID):
+            url = 'https://api.themoviedb.org/3/discover/movie?api_key=' + self.key + '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres='
             if ' ' in keywordID:
-                keywordID.replace(' ', '%2C')
-            search = url + keywordID
+                keywordID.replace(' ','%2C')
+            search = url + genreID
+            search = search + "&with_keywords=" + keywordID
             return requests.get(search).json()
 
-        def simpleSearch(self, keyword):
+        def simpleSearch(self,genre,keyword):
+            title="NO MOVIE FOUND"
             keyWordID = ""
+            genreID=""
+            genreID+=str(self.searchGenre(genre))
             for y in keyword:
                 key = self.searchKeyword(y)
                 res = key['results']
                 id = 0
                 for x in res:
-                    # compare case insensitive
+                    #compare case insensitive
                     if x['name'].casefold() == y.casefold():
                         id = x['id']
-                        # print(id)
+                        #print("keyword: ", y)
+                        #print("keywordID: ", id)
                         keyWordID += str(id) + ","
 
-                movieList = self.discover(keyWordID)
+            movieList = self.discover(genreID,keyWordID)
+            if (movieList.get("total_results")!=0):
+                title = movieList['results'][0]['title']
 
-            return movieList['results'][0]['title']
+            return title
 
     test = Tmdb("6ca5bdeac62d09b1186aa4b0fd678720")
     keyword = keywords
-    output = test.simpleSearch(keyword)
+    output = test.simpleSearch(genre, keyword)
     return output
